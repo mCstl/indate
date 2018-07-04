4a513f6979e7cbc7222dd6c32a0ace7debd8d03a
mCstl <37896336+mCstl@users.noreply.github.com>
2018-07-05 01:33:38 +0300

    Update first.sh

--- first.sh
+++ first.sh
@@ -4,3 +4,10 @@ cd /etc/ssh/
 mkdir insecure_origin_keys
 mv ssh_host_* insecure_origin_keys/
 dpkg-reconfigure openssh-server
+echo ''
+echo '==============='
+echo 'next /etc/default/grub will be open, change GRUB_TIMEOUT to 0'
+echo 'press [ENTER]'
+read ok
+nano /etc/default/grub
+update-grub
