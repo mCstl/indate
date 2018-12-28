454e3d5711ad72c21d06b661d3fa079acbc5696b
mCstl <37896336+mCstl@users.noreply.github.com>
2018-12-28 18:03:37 +0000

    Create live.sh

---
+++ live.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+add-apt-repository ppa:jonathonf/python-3.6
+apt update -y
+apt install python2.7-dev -y
+apt install python3.6 -y
+apt install python3.6-dev -y
+apt install python-pip -y
+apt install python3-pip -y
+apt install python-httplib2 -y
+apt install python3-httplib2 -y
+apt install python-http-parser -y
+#!!!apt install python3-lxml python3-lxml-dbg -y
+sudo apt install npm -y
+sudo apt install libxml2 libxml2-dev libxslt1.1 libxslt1-dev -y
+echo
+echo '_______________________________________'
+echo
+echo '---------------------------------------'
+echo '_______________________________________'
+pip install --update
+pip3 install --update
+echo 'creating dependencies SSH'
+apt install python-paramiko -y
+echo '---------------------------------------'
+echo 'creating dependencies HTTP'
+apt install python-pycurl curl -y
+echo '---------------------------------------'
+echo "creating dependencies AJP"
+pip install ajpy
+echo '---------------------------------------'
+echo "creating dependencies LDAP"
+apt install libldap-2.4-2 -y
+echo '---------------------------------------'
+echo"creating dependencies SMB"
+apt install python-impacket -y
+echo '---------------------------------------'
+echo "creating dependencies Oracle"
+python -m pip install cx_Oracle --upgrade
+echo '---------------------------------------'
+echo "creating dependencies MySQL"
+apt install python-mysqldb -y
+echo '---------------------------------------'
+echo "creating dependencies RDP (NLA)"
+apt install freerdp2-x11 -y
+echo '---------------------------------------'
+echo "creating dependencies PostgreSQL"
+apt install python-psycopg2 -y
+echo '---------------------------------------'
+echo "creating dependencies VNC"
+apt install python-crypto -y
+echo '---------------------------------------'
+echo "creating dependencies DNS"
+apt install python-dns -y
+echo '---------------------------------------'
+echo "creating dependencies NET keyword"
+apt install python-ipy -y
+echo '---------------------------------------'
+echo "creating dependencies SNMP"
+apt install python-pyasn1 python-pysnmp4 -y
+echo '---------------------------------------'
+echo "creating dependencies IKE"
+apt install ike-scan -y
+echo '---------------------------------------'
+echo "ZIP pass"
+apt install unzip -y
+echo '---------------------------------------'
+echo "keystore files"
+apt install default-jre -y
+echo '---------------------------------------'
+echo "creating dependencies SQLCipher"
+apt install libsqlite3-dev libsqlcipher-dev -y
+pip install pysqlcipher
+echo '---------------------------------------'
+echo "installing libxslt"
+aptinstall libxml2-dev libxslt-dev
+echo '_______________________________________'
+echo
+echo '---------------------------------------'
+echo '_______________________________________'
+echo "creating dependencies pysqlcipher ajpy"
+pip install pysqlcipher ajpy
+echo '---------------------------------------'
+python -m pip install cx_Oracle --upgrade
+easy_install pip==7.1.2
+echo ""
+echo "  ___                     _         _                 _  _  ___  "
+echo " | _ \_ _ _____ ___  _ __| |_  __ _(_)_ _  ___  ___  | \| |/ __| "
+echo " |  _/ '_/ _ \ \ / || / _| ' \/ _' | | ' \(_-< |___| | .' | (_ | "
+echo " |_| |_| \___/_\_\\\_, \__|_||_\__,_|_|_||_/__/       |_|\_|\___| "
+echo "                  |__/                                           "
+echo ""
+apt remove proxychains -y
+cd /opt/
+git clone https://github.com/rofl0r/proxychains-ng.git
+cd proxychains-ng/
+chmod +x ./configure
+bash ./configure --prefix=/usr --sysconfdir=/etc
+make && make install
+make install-config
+./tools/install.sh -D -m 644 src/proxychains.conf /etc/proxychains.conf
+./tools/install.sh -D -m 644 libproxychains4.so /usr/lib/libproxychains4.so
+./tools/install.sh -D -m 755 proxychains4 /usr/bin/proxychains4
+cd ~/
+mkdir git
+cd git
+git clone https://github.com/m4ll0k/DeepSearch.git
+git clone https://github.com/aboul3la/Sublist3r.git
+git clone https://github.com/andresriancho/w3af.git
+cd w3af
+./w3af_console
+npm install -g retire
+/tmp/w3af_dependency_install.sh
